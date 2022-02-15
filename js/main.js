b9b5e036ac2f12babababef997e7786fba1a79ee
Jacob Yi <96788421+jacobyi11@users.noreply.github.com>
2022-02-14 18:54:00 -0800

    Listen for 'submit' events on the journal entry form and:

--- js/main.js
+++ js/main.js
@@ -2,10 +2,28 @@
 /* exported data */
 
 var $photoUrl = document.querySelector('.photo-url');
+$photoUrl.addEventListener('input', getURL);
+
 var $photo = document.querySelector('.photo');
 
-$photoUrl.addEventListener('input', getURL);
+var $form = document.querySelector('form');
+$form.addEventListener('submit', formData);
+
+var $inputs = document.querySelector('form').elements;
 
 function getURL(event) {
   $photo.setAttribute('src', event.target.value);
+}
+
+function formData(event) {
+  event.preventDefault();
+  var obj = {};
+  for (var i = 0; i < $inputs.length; i++) {
+    obj[$inputs[i].name] = $inputs[i].value;
+  }
+  obj.nextEntryId = data.nextEntryId;
+  data.nextEntryId++;
+  data.entries.unshift(obj);
+  $photo.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $form.reset();
 }
